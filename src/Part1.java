408b91fd1a02cd4463a3f9a269820aa5fb34267b
PhilMcGee <philip.mcgee@my.jcu.edu.au>
2015-03-09 15:53:43 +1000

    Initiate Part 1

--- src/Part1.java
+++ src/Part1.java
@@ -1,7 +1,12 @@
 /**
  * Created by jc143867 on 9/03/15.
  */
-public class Part1 {
+//**
+// * Text viewer App - prints the contents of a string to the console
+// *   Created by Phil McGee 9/3/2015
+// */
+
+ public class Part1 {
     public static void main(String[] args){
         String text = "This is a simple test\nOne Two Three\nHello world!";
         for (int i=0; i < text.length(); ++i){
